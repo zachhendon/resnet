203ed99037870c2c7d2c9d921329905ceb68cd74
ZachHendon <zhendon@wisc.edu>
2024-04-14 18:36:16 -0500

    Implemented data loader

---
+++ data/dataset.py
@@ -0,0 +1,42 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def data_loader(
+        data_dir,
+        batch_size,
+        val_size=0.1,
+        shuffle=True,
+        test=False
+):
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    if test:
+        test_dataset = datasets.CIFAR10(
+            root=data_dir, train=False,
+            download=True, transform=transform
+        )
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
+        return test_loader
+
+    train_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform
+    )
+
+    num_train = len(train_dataset)
+    val_split = int(val_size * num_train)
+    train_split = num_train - val_split
+    train_subset, val_subset = random_split(train_dataset, [train_split, val_split])
+
+    train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=shuffle)
+    val_loader = DataLoader(val_subset, batch_size=batch_size, shuffle=shuffle)
+
+    return train_loader, val_loader
