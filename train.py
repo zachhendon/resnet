f49750fd04d1941dea7ad0ea312ac958b6740fe0
ZachHendon <zhendon@wisc.edu>
2024-04-17 22:52:56 -0500

    Added arguments to train.py and improved print statements

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
 import torch
-import torch.nn as nn
 from data.dataset import data_loader
-from model import ResNet
+from model import *
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
-import torch.backends.cuda as cudnn
+import argparse
 
 def train_epoch(model, loss_fn, optimizer, scheduler):
     running_loss = 0
@@ -49,12 +48,8 @@ def val_epoch(model, loss_fn):
         avg_acc = num_correct / num_val
         return avg_loss, avg_acc
 
-def train(model, loss_fn, optimizer, scheduler, epochs, model_name):
+def train(model, loss_fn, optimizer, scheduler, epochs, model_name, writer):
     best_val_loss = torch.inf
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    writer = SummaryWriter(f"models/{model_name}_{timestamp}")
-
-    print("Starting training...")
 
     for epoch in range(epochs):
         model.train(True)
@@ -78,25 +73,60 @@ def train(model, loss_fn, optimizer, scheduler, epochs, model_name):
             model_path = f"models/{model_name}_{timestamp}/checkpoint_{epoch}"
             torch.save(model.state_dict(), model_path)
 
-    torch.save(model.state_dict(), f"models/{model_name}_{timestamp}/best_model")
+    best_model_path = f"models/{model_name}_{timestamp}/best_model"
+    torch.save(model.state_dict(), best_model_path)
+    print(f"Saved best model to {best_model_path}")
 
+# Get model from args
+models = {
+    "resnet18": get_resnet18,
+    "resnet34": get_resnet34
+}
+parser = argparse.ArgumentParser(description="train.py is the program to train the CIFAR-10 dataset on a ResNet model")
+parser.add_argument('-m', '--model',
+                    action='store',
+                    type=str,
+                    choices=models.keys(),
+                    default='resnet18',
+                    help="the model to train - resnet18 (default) or resnet34",
+                    metavar="")
+def size_range(value):
+    fvalue = float(value)
+    if fvalue < 0.01 or fvalue > 1:
+        raise argparse.ArgumentTypeError("%r is not in the range [0.01, 1]" % value)
+    return fvalue
+parser.add_argument('-s', '--size',
+                    action='store',
+                    default=1,
+                    type=size_range,
+                    help="the percent of the dataset to use for training - range=[0.01, 1]",
+                    metavar="")
+args = parser.parse_args()
+model_name = args.model
+subset_ratio = args.size
+print(f"Selected model: {model_name}")
 
+# Load data
 train_loader, val_loader = data_loader(
     data_dir='./data/datasets', batch_size=128,
-    subset_ratio=1
+    subset_ratio=subset_ratio
 )
 num_training = sum(len(inputs) for inputs, _ in train_loader)
 num_val = sum(len(inputs) for inputs, _ in val_loader)
 
-model = ResNet([3, 4, 6, 3]).cuda()
-model = nn.DataParallel(model)
-cudnn.benchmark = True
+# Load model
+model_fn = models[model_name]
+model = model_fn()
 
+# Initialize loss, optimizer, and schedule
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[90, 135], gamma=0.25)
 EPOCHS = 180
 
-model_name = 'resnet34'
+# Begin training
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+writer = SummaryWriter(f"models/{model_name}_{timestamp}")
 
-train(model, loss_fn, optimizer, scheduler, EPOCHS, model_name)
+print("Starting training")
+train(model, loss_fn, optimizer, scheduler, EPOCHS, model_name, writer)
